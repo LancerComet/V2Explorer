3bd736d41ddb4f2e87a3c2bf9e2033313fa64d9a
LancerComet <chw644@hotmail.com>
2017-07-05 17:37:23 +0800

    + WIP.

--- V2EX-UWP/Views/Page.Topic.xaml.cs
+++ V2EX-UWP/Views/Page.Topic.xaml.cs
@@ -17,9 +17,33 @@ using Windows.UI.Xaml.Navigation;
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
 namespace V2EX.Views {
+  public class Node {
+    public string name { get; set; }
+    public string label { get; set; }
+
+    public Node (string name, string label) {
+      this.name = name;
+      this.label = label;
+    }
+  }
+
+  public class ViewModel {
+    public List<Node> nodes {
+      get {
+        return new List<Node>() {
+          new Node("all", "全部"),
+          new Node("hot", "最热"),
+          new Node("tech", "技术"),
+          new Node("creative", "创意")
+        };
+      }
+    }
+  }
+
   public sealed partial class TopicPage : Page {
     public TopicPage () {
       this.InitializeComponent();
+      DataContext = new ViewModel();
     }
 
     public string pageLabel {
